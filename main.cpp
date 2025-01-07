ad34daaed7102d840f6714791c3bb03dada61920
LuizGirotto <luiz.gustavo.bernardes@uel.br>
2025-01-06 21:50:17 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,1 @@
-bool excluirDaFila(FILA* f, REGISTRO* reg) {
-    if (f->inicio->prox == NULL) return false;  // Fila vazia
-
-    PONT apagar = f->inicio->prox;
-    *reg = apagar->reg;
-
-    f->inicio->prox = apagar->prox;
-    if (f->inicio->prox == NULL) {  // Caso a fila fique vazia
-        f->fim = NULL;
-    }
-
-    free(apagar);
-    return true;
-}
 
